29a2013328b04740e357bd6d09ac51f0f2b05ea6
Brandon Luna Rodriguez <disrespectings@gmail.com>
2021-06-21 17:40:06 -0700

    fixed resume errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,23 +7,11 @@ const NavBar = () => {
         <div>
             <nav className='navbar'>
             <a className='logo' href="#">B</a>
-            {/* Mobile Nav */}
-            {/* <div className="mobile-menu-icons">
-                <input type="checkbox" id='nav_bars' className='nav_bars' />
-                <label for='nav_bars' className='nav_toggle'>
-                <FontAwesomeIcon icon={faBars} size='2x' id='nav_bars' className='example linkedin' />
-                </label>
-            </div> */}
                 <ul className='menu'>
                     <li className='item'> <a href="#skills">SKILLS</a></li>
                     <li className='item'> <a href="#projects">PROJECTS</a></li>
                     <li className='item'> <a href="#contact-me">CONTACT ME</a></li>
                 </ul>
-                {/* <ul className='mobile-menu'>
-                    <li className='mobile-item'> <a href="#skills">SKILLS</a></li>
-                    <li className='mobile-item'> <a href="#projects">PROJECTS</a></li>
-                    <li className='mobile-item'> <a href="#contact-me">CONTACT</a></li>
-                </ul> */}
             </nav>
         </div>
     )
