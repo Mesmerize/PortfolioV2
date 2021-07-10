8529c5eaa948d75a916cc5e7196a8a7ba3bbe1e1
Brandon Luna Rodriguez <disrespectings@gmail.com>
2021-07-10 05:08:58 -0700

    Added new images to projects section and added hover effect on project containers

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,11 @@
-import React, {useEffect} from 'react'
-import pic from '../img/test.png'
-import pic2 from '../img/test2.png'
-import pic3 from '../img/test3.png'
-import pic4 from '../img/test4.png'
-import pic5 from '../img/test5.png'
+import React, {useEffect} from 'react';
+import pic from '../img/colours.jpg';
+import pic2 from '../img/test2.png';
+import pic3 from '../img/star.jpg';
+import pic4 from '../img/weather.jpg';
+import pic5 from '../img/ecomm.jpg';
+import pic6 from '../img/conversable.jpg';
+import pic7 from '../img/student.jpg';
 
 import Aos from 'aos'
 import 'aos/dist/aos.css'
@@ -25,6 +27,24 @@ const Projects = () => {
                 </p>
                 <div id="project-container">
                     <div className="main-project-container">
+                        <img src={pic6} className='proj-img' alt="" />
+                        <h1 className='project-title'>Conversable</h1>
+                        <p className='project-desc'>Peer to Peer React Video Application to encourage socializing and productivity with friends. Utilized WebRTC and Web Sockets to get feed from users.</p>
+                        <div className='links'>
+                            <a href="https://conversable.netlify.app/" target='_blank'><p className='demo'>View Live Demo &gt;</p></a>
+                            <a href="https://github.com/Mesmerize/Conversable" target='_blank' className='spaced'><p className='demo'>View Source Code &gt;</p></a>
+                        </div>
+                    </div>
+                    <div className="main-project-container">
+                        <img className='proj-img' src={pic7} alt="" />
+                        <h1 className='project-title'>Student Profiles</h1>
+                        <p className='project-desc'>React Application built for a front end assessment challenge that utilizes axios to retrieve student data from an API and allows user to filter by name.</p>
+                        <div className='links'>
+                            <a href="https://studentprofilesproj.netlify.app/" target='_blank'><p className='demo'>View Live Demo &gt;</p></a>
+                            <a href="https://github.com/Mesmerize/StudentProfiles" target='_blank' className='spaced'><p className='demo'>View Source Code &gt;</p></a>
+                        </div>
+                    </div>
+                    <div className="main-project-container">
                         <img src={pic} className='proj-img' alt="" />
                         <h1 className='project-title'>Colors Landing Page</h1>
                         <p className='project-desc'>Colors landing page built with pure HTML, CSS, and JavaScript. Which helped practice my CSS layout abilities and design</p>
@@ -45,7 +65,7 @@ const Projects = () => {
                     <div className="main-project-container">
                     <img src={pic3} className='proj-img' alt="" />
                         <h1 className='project-title'>Starbucks Landing Page</h1>
-                        <p className='project-desc'>Clone of a Starbucks Landing Page, built and designed with HTML, CSS, and JavaScript to mimick item changing features.</p>
+                        <p className='project-desc'>Clone of a Starbucks Landing Page, built and designed with HTML, CSS, and JavaScript to replicate item changing features.</p>
                         <div className='links'>
                             <a href="https://mesmerize.github.io/Starbs/" target='_blank'><p className='demo'>View Live Demo &gt;</p></a>
                             <a href="https://github.com/Mesmerize/Starbs" target='_blank' className='spaced'><p className='demo'>View Source Code &gt;</p></a>
@@ -54,7 +74,7 @@ const Projects = () => {
                     <div className="main-project-container">
                     <img className='proj-img' src={pic4} alt="" />
                         <h1 className='project-title'> React Weather App</h1>
-                        <p className='project-desc'>React Weather Application, built with HTML, CSS, React, and Node JS. Which allows users to get weather updates from anywhere in the world.</p>
+                        <p className='project-desc'>Weather Application, built with HTML, CSS, and Node Js. Which allows users to get weather updates from anywhere in the world.</p>
                         <div className='links'>
                             <a href="https://brandonsweatherapp.netlify.app/" target='_blank'><p className='demo'>View Live Demo &gt;</p></a>
                             <a href="https://github.com/Mesmerize/WeatherApp" target='_blank' className='spaced'><p className='demo'>View Source Code &gt;</p></a>
